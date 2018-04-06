adc5a5896c8de1fb0251c3220f8f082af266a8cb
root <root@ssd-desktop.(none)>
2018-04-06 21:37:46 +0900

    NoDAB

---
+++ blueftl_lab1_1/src/blueftl_gc_page.h
@@ -0,0 +1,13 @@
+#ifndef _BLUESSD_GC_PAGE
+#define _BLUESSD_GC_PAGE
+
+#include <linux/types.h>
+#include "blueftl_ftl_base.h"
+
+/* TODO: 그냥 NO 답 */
+int32_t gc_page_trigger_gc_lab(
+		struct ftl_context_t* ptr_ftl_context, 
+		uint32_t gc_target_bus, 
+		uint32_t gc	target_chip);
+
+
